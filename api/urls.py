f74e6382e567d14ffb942c31df003b2b847bb19e
taniacruzz <tanicruz112@gmail.com>
2023-05-23 19:28:03 -0300

    exclusão de comentários

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
     path('v3/places/update/:<int:id>', views.updatePlace),
     path('v3/places/delete/:<int:id>', views.deletePlace),
     path('v4/places/:<int:id1>/distancia/:<int:id2>', views.distance),
-    # path('v4/search/latitude={latitude}&longitude={longitude}&radius={raio}', views.placesInRatio),
     path('v4/search/', views.placesInRatio),
-    # path('v4/search?latitude:<str:latitude>&longitude:<str:longitude>&radius:<str:radius>', views.placesInRatio)
 ]
